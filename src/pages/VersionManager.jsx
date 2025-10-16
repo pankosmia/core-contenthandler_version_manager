9176f817d4053982ca5c8c2b18397ace04e609c1
lmeline <loise13@hotmail.fr>
2025-10-16 14:56:13 +0200

    deplacement complet du version manager

--- src/pages/VersionManager.jsx
+++ src/pages/VersionManager.jsx
@@ -1,19 +1,20 @@
 import { useState, useContext, useEffect } from 'react';
 import {
-    Drawer,
     Box,
     Tabs,
     Tab,
-    Typography
+    Typography,
+    Dialog,
+    DialogActions,
+    Button
 } from "@mui/material";
 import PropTypes from 'prop-types';
-import { debugContext, i18nContext, doI18n, getJson } from "pithekos-lib";
+import { debugContext, i18nContext, doI18n, getJson, Header } from "pithekos-lib";
 import ChangesTab from './ChangesTab';
 import SettingsTab from './SettingsTab';
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
-
     return (
         <div
             role="tabpanel"
@@ -48,15 +49,19 @@ function VersionManager() {
     const [repoPath, setRepoPath] = useState([]);
     const [open, setOpen] = useState(true);
     const [repoName, setRepoName] = useState([]);
-
-    const toggleDrawer = (newOpen) => () => {
-        setOpen(newOpen);
-    };
+    const [fullWidth, setFullWidth] = useState(true);
+    const [maxWidth, setMaxWidth] = useState('lg');
 
     const handleTabsChange = (event, newValue) => {
         setTabValue(newValue);
     };
 
+    const handleClose = () => {
+       setOpen(false);
+        setTimeout(() => {
+            window.location.href = '/clients/content';
+        }, 300);
+    }
     const getProjectSummaries = async () => {
         const hash = window.location.hash;
         const query = hash.includes('?') ? hash.split('?')[1] : '';
@@ -80,47 +85,80 @@ function VersionManager() {
         },
         []
     );
-    return <Box>
-        <Drawer
-            anchor="right"
-            open={open}
-            onClose={toggleDrawer(false)}
-        >
-            <Box sx={{ p: 1, m: 1, width: '45vh' }}>
-                <Typography variant="h6" component="div" fontWeight="bold">{doI18n("pages:content:version_manager", i18nRef.current)}</Typography>
-                <Typography variant="subtitle1" component="div" fontWeight="bold"> {repoName} </Typography>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs
-                        value={tabValue}
-                        variant="fullWidth"
-                        onChange={handleTabsChange}
-                        aria-label="version manager tabs"
-                        centered
-                    >
-                        <Tab label={doI18n("pages:content:changes_tab", i18nRef.current)} {...a11yProps(0)} />
-                        <Tab label={doI18n("pages:content:settings_tab", i18nRef.current)} {...a11yProps(1)} />
-                    </Tabs>
+    return (
+        <Box>
+            <Box
+                sx={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    zIndex: -1,
+                    backgroundImage:
+                        'url("/app-resources/pages/content/background_blur.png")',
+                    backgroundRepeat: "no-repeat",
+                }}
+            />
+            <Header
+                titleKey="pages:content:title"
+                currentId="content"
+                requireNet={false}
+
+            />
+            <Dialog
+                fullWidth={fullWidth}
+                maxWidth={maxWidth}
+                open={open}
+                onClose={handleClose}
+                sx={{
+                    backdropFilter:"blur(3px)"
+                }}
+            >
+                <Box sx={{padding:1}}>
+                    <Typography variant="h6" component="div" fontWeight="bold">{doI18n("pages:content:version_manager", i18nRef.current)}</Typography>
+                    <Typography variant="subtitle1" component="div" fontWeight="bold"> {repoName} </Typography>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs
+                            value={tabValue}
+                            //variant="fullWidth"
+                            onChange={handleTabsChange}
+                            aria-label="version manager tabs"
+                            //centered
+                        >
+                            <Tab label={doI18n("pages:content:changes_tab", i18nRef.current)} {...a11yProps(0)} />
+                            <Tab label={doI18n("pages:content:settings_tab", i18nRef.current)} {...a11yProps(1)} />
+                        </Tabs>
+                    </Box>
+                    <CustomTabPanel value={tabValue} index={0}>
+                        <ChangesTab
+                            repoPath={repoPath}
+                            repoName={repoName}
+                            open={tabValue === 0}
+                            setTabValue={setTabValue}
+                            setRemoteUrlExists={setRemoteUrlExists}
+                        />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={tabValue} index={1}>
+                        <SettingsTab
+                            repoInfo={repoPath}
+                            open={tabValue === 1}
+                            remoteUrlExists={remoteUrlExists}
+                            setRemoteUrlExists={setRemoteUrlExists}
+                        />
+                    </CustomTabPanel>
                 </Box>
-                <CustomTabPanel value={tabValue} index={0}>
-                    <ChangesTab
-                        repoPath={repoPath}
-                        repoName={repoName}
-                        open={tabValue === 0}
-                        setTabValue={setTabValue}
-                        setRemoteUrlExists={setRemoteUrlExists}
-                    />
-                </CustomTabPanel>
-                <CustomTabPanel value={tabValue} index={1}>
-                    <SettingsTab
-                        repoInfo={repoPath}
-                        open={tabValue === 1}
-                        remoteUrlExists={remoteUrlExists}
-                        setRemoteUrlExists={setRemoteUrlExists}
-                    />
-                </CustomTabPanel>
-            </Box>
-        </Drawer>
-    </Box>
+                 <DialogActions>
+                    <Button
+                        onClick={handleClose}
+                        color='primary'
+                    >
+                        {doI18n("pages:content:close", i18nRef.current)}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Box>
+    );
 }
 
 export default VersionManager;
