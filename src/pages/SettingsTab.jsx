c3ea3c68dd01c906d07a5973db0236d4bd9686ea
eliaspinero <eliasrpt14@gmail.com>
2025-10-17 10:01:59 -0400

    Fixed the styling.

--- src/pages/SettingsTab.jsx
+++ src/pages/SettingsTab.jsx
@@ -180,7 +180,7 @@ function SettingsTab({repoInfo, open, reposModCount, remoteUrlExists, setRemoteU
         setSelectedBranchIndex(index);
     };
 
-    return <Box> 
+    return <Box sx={{ height: "70vh" }}> 
             <Stack spacing={2}>
                 <Box sx={{display: 'flex', flexDirection: 'row', justifyContent:"flex-end"}}>
                     <TextField
